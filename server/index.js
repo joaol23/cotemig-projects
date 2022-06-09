fe97d71e30761aa1b4023301a02e2cccd7527369
joaol23 <joaolucas.m.ferreira2008@gmail.com>
2022-06-09 01:18:15 -0300

    teste

--- server/index.js
+++ server/index.js
@@ -1,7 +1,6 @@
 import { createRequire } from 'module'
 const require = createRequire(import.meta.url);
 
-const path = require('path');
 const express = require("express");
 
 import { checkFilesExists, getFiles } from './handleFiles/readFiles.js';
@@ -9,7 +8,6 @@ import { checkFilesExists, getFiles } from './handleFiles/readFiles.js';
 const PORT = process.env.PORT || 3001;
 
 const app = express();
-app.use(express.static(path.resolve(__dirname, '../client/build')));
 
 app.get("/api", (req, res) => {
   res.json({ message: "Hello from server!" });
