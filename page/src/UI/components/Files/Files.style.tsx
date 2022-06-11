3690b654ff4ba5cde7ec6a0968d1b4d4acba4c7d
joaol23 <joaolucas.m.ferreira2008@gmail.com>
2022-06-11 00:31:21 -0300

    concertando layout

--- page/src/UI/components/Files/Files.style.tsx
+++ page/src/UI/components/Files/Files.style.tsx
@@ -3,6 +3,7 @@ import { styled } from "@mui/material";
 export const ContainerAll = styled('div')`
     width: 100%;
     max-width: 1280px;
+    margin: 0 auto;
 `;
 
 export const FilesContainer = styled('div')`
