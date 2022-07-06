fe9318bb3dcad9a8cf3bcdeeba8ca1893710c4da
JOAO_LUCAS_MESQUITA_FERREIRA <12101664@OMEGAB22.BARROCA>
2022-07-06 11:29:29 -0300

    Melhorando rotas

--- server/routes/pokemons.js
+++ server/routes/pokemons.js
@@ -1,5 +1,6 @@
 import { apiPokemonUrl, limitPokemonsPage } from '../database/api/config.js'
 import fetch from 'node-fetch';
+var error;
 
 
 // export const routes = [
@@ -7,25 +8,57 @@ import fetch from 'node-fetch';
 // ]
 
 export function routePokemon(app) {
-    app.get("/pokemons", async (req, res) => {
+    app.get("/api/pokemons", async (req, res) => {
         const pokemons = await getPokemons();
-        res.json({ pokemons: pokemons })
+        if (pokemons) {
+            res.status(200).json({ data: pokemons })
+            return;
+        }
+
+        res.sendStatus(error.status);
     })
 
-    app.get("/pokemon/:name", async (req, res) => {
+    app.get("/api/pokemon/:name", async (req, res) => {
         const pokemonName = req.params.name;
-        res.send(pokemonName);
+        const pokemon = await getPokemonByName(pokemonName);
+        if (pokemon) {
+            res.status(200).json({ data: pokemon });
+            return;
+        }
+
+        res.sendStatus(error.status);
     })
 }
 
 async function getPokemons() {
     let dataPokemons = await fetch(`${apiPokemonUrl}pokemon?limit=${limitPokemonsPage}&offset=0`);
-    dataPokemons = await dataPokemons.json();
+    if (dataPokemons.status == 200) {
+        dataPokemons = await dataPokemons.json();
 
-    dataPokemons = dataPokemons.results.map(async (pokemon) => {
-        let eachPokemonData = await (await fetch(pokemon.url)).json();
-        return eachPokemonData;
-    })
+        dataPokemons = dataPokemons.results.map(async (pokemon) => {
+            let eachPokemonData = await (await fetch(pokemon.url)).json();
+            return eachPokemonData;
+        })
 
-    return await Promise.all(dataPokemons);
+        return await Promise.all(dataPokemons);
+    }
+
+
+    setError({ status: dataPokemons.status, message: dataPokemons.statusText });
+    return false;
 }
+
+async function getPokemonByName(name) {
+    let pokemon = await fetch(`${apiPokemonUrl}pokemon/${name}`);
+    if (pokemon.status == 200) {
+        return await pokemon.json();
+    }
+
+    setError({ status: pokemon.status, message: pokemon.statusText });
+    return false;
+}
+
+function setError(err) {
+    error = err;
+}
+
